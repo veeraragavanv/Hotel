4ade93a8b955d3407f15b44e8abe3ffcce1e3c65
veeraragavanv <ragavan.cse3@gmail.com>
2015-05-26 22:17:20 +0530

    Basic Routes

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,26 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET users listing. */
+router.get('/create', function(req, res, next) {
+  var vm = {
+    title: 'Create an Account'};
+  res.render('users/create', vm);
+});
+
+/* GET users listing. */
+router.post('/create', function(req, res, next) {
+  var SomeThingGoesWrong = false;
+  if(SomeThingGoesWrong){
+    var vm = {
+      title: 'Create an Account',
+      input: req.body,
+      error: 'SomethingWentWrong'
+    };
+    delete  vm.input.password;
+    return res.render('users/create', vm);
+  }
+  res.redirect('/orders');
+});
+
 module.exports = router;
